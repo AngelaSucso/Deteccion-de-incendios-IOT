9f31e40fcd889887f7778a8592e1845e1465cf12
EvelynLizbeth <ecusiha@unsa.edu.pe>
2025-12-18 18:08:28 -0500

    Mensaje en telegram

---
+++ telegram_message.py
@@ -0,0 +1,25 @@
+import requests
+
+TOKEN = "8510166570:AAHLXgXM0_EUQAneW-XLSnQHdCeyMYYN0KQ"
+CHAT_ID = "8532744967"
+MENSAJE = "¡Alerta! Incendio detectado 🔥"
+foto_path = "foto_incendio.jpg"
+
+def enviar_alerta_telegram(foto_path, mensaje):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
+
+    with open(foto_path, "rb") as foto:
+        files = {"photo": foto}
+        data = {
+            "chat_id": CHAT_ID,
+            "caption": MENSAJE
+        }
+        
+        # Enviar la foto con el mensaje
+        response = requests.post(url, files=files, data=data)
+
+    # Verifica si el mensaje fue enviado correctamente
+    if response.status_code == 200:
+        print("Foto enviada correctamente")
+    else:
+        print(f"Error al enviar la foto: {response.text}")
